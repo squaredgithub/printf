22c097877e70faa9b329bd49ff8f31b8df07072b
Musty-Mode <mustafayare1001@gmail.com>
2023-01-26 11:12:53 -0800

    plurals

--- get_flags.c
+++ get_flags.c
@@ -3,7 +3,7 @@
 /**
  * get_flags - Calculates active flags
  * @format: Formatted string in which to print the arguments
- * @i: take a parameter.
+ * @i: takes a parameter.
  * Return: Flags:
  */
 int get_flags(const char *format, int *i)
